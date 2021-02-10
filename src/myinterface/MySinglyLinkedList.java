a7d9a25fbcf3a80eaeb4e722e81344bf77ce8a88
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-10 10:57:56 +0530

    declared some method in MySinglyLinkedList

--- src/myinterface/MySinglyLinkedList.java
+++ src/myinterface/MySinglyLinkedList.java
@@ -9,4 +9,14 @@ public interface MySinglyLinkedList {
     //void addLast(int element);
     boolean isEmpty();// will give true if linked list is empty
     int size();//will give total number of elements in linked list
+    void addLast(int element);
+    void addLastWithoutUsingTail(int element);
+    void traverse();
+    void addAfterGivenNode(int givenElement, int element);
+    //void addBeforeGivenNode(int givenElement, int element);
+    int removeFirst();
+    int removeLast();
+    int removeGivenNode(int givenElement);
+    boolean search(int element);
+
 }
